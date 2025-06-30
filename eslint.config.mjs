3088d83b21ee396ab2ad8df16fe02847c580199b
mca <abutimartin778@gmail.com>
2025-06-30 16:32:36 +0300

    c1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            // Allow unused variables/args if they start with "_"
+            "@typescript-eslint/no-unused-vars": ["warn", {
+                varsIgnorePattern: "^_",
+                argsIgnorePattern: "^_",
+                ignoreRestSiblings: true
+            }],
+            "@typescript-eslint/no-explicit-any": "off",
+            "prefer-const": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@next/next/no-html-link-for-pages": "off",
+            "@typescript-eslint/no-require-imports": "off",
+            "react/no-unescaped-entities": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+        },
+    },
 ];
 
 export default eslintConfig;
