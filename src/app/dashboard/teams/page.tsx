92ad0e1f8733e5b6de58e3207adad13696696ad9
mca <abutimartin778@gmail.com>
2025-07-04 12:13:15 +0300

    c1

--- src/app/dashboard/teams/page.tsx
+++ src/app/dashboard/teams/page.tsx
@@ -212,7 +212,7 @@ export default function TeamsPage() {
           ) : (
             <Grid container spacing={2}>
               {userTeams.map((team) => (
-                <Grid key={team.id} item xs={12} md={6}>
+                <Grid key={team.id}  sx={{xs:12,md:6}}>
                   <Card sx={{ 
                     borderRadius: 2,
                     boxShadow: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
